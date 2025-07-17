66297b5b64702506676b55dfe0ea1b26dcfbbbea
Sunny <sgandhari06@gmail.com>
2025-07-17 15:28:47 -0500

    Initial portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  if (window.particlesJS) {
+    particlesJS.load('particles-js', 'particles.json', function() {
+      // callback
+    });
+  }
+}); 
